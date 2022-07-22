1afaf7ef2a2032961ecf97a555104549e471c533
Preston Darrough <techdarrough@gmail.com>
2022-07-21 20:28:17 -0700

    first commit

---
+++ app/projects/schema.py
@@ -0,0 +1,13 @@
+from marshmallow import Schema, fields
+
+class GitHubRepoSchema(Schema):
+    id = fields.Int(required=True)
+    repo_name = fields.Str()
+    full_name = fields.Str()
+    language = fields.Str()
+    description = fields.Str()
+    repo_url = fields.URL()
+
+class ProjectSchema(GitHubRepoSchema):
+    user_id = fields.Email(required=True)
+    
